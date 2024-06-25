717135500ad1302f65a6f4b84a953e60f3076193
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2024-06-25 07:20:20 +0300

    Initial commit

---
+++ RshowMiddleware/Program.cs
@@ -0,0 +1,10 @@
+﻿const string PATH = "C:/Users/FlooferLand/Desktop/Old_Time_Rock_and_Roll.rshw";
+
+rshwFormat? show = rshwFormat.ReadFromFile(PATH);
+if (show != null) {
+    foreach (int bit in show.signalData) {
+        Console.Write($"{bit} ");
+    }
+} else {
+    Console.WriteLine($"Show file at \"{PATH}\" is null");
+}
